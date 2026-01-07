0c701ef3d6c7bc0ff1b0b6c9e0c4df8df0b8b4f6
alexey <alexey808sv@gmail.com>
2026-01-07 20:15:17 +0400

    home work 3

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import './Layout.css';
 import Header from '../Header/Header';
 import Search from '../Search/Search';
+import Movies from '../Movies/Movies';
 
 function Layout() {
   return (
     <div className="layout">
       <Header/>
       <Search/>
+      <Movies/>
     </div>
   );
 }
